4f8a2638a724fe7fb512b2675e7800925b6f9f4f
Mark Nutter <manutter51@gmail.com>
2013-09-21 16:22:35 -0400

    See if :dev-dependencies makes Travis any happier

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [midje "1.6-beta1"]]
+  :dev-dependencies [[lein-midje "3.1.1"]] ;; seems to be needed for
+  ;; Travis CI :/
   :profile {:dev {:plugins [[lein-midje "3.1.1"]]}})
